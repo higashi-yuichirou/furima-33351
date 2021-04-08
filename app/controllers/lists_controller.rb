ae5c5737c99e7885691006d348deb475c43f4002
higashi-yuichirou <0fw3251t0240c5w@ezweb.ne.jp>
2021-04-08 20:13:06 +0900

    コード修正

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:edit, :show, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @lists = List.order('created_at DESC')
